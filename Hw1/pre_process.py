14d1fbb4200ff07977f04becf6f2ffb80538678a
Yoyo <b850108cd@gmail.com>
2019-10-09 16:32:46 +0800

    extract util

--- Hw1/pre_process.py
+++ Hw1/pre_process.py
@@ -2,71 +2,10 @@ import abc
 import json
 from pathlib import Path
 
+from util import ListLoader, TermLoader
+
 DATA_PATH = Path(__file__).parent / 'Data'
 DOCUMENT_PATH = Path(__file__).parent / 'Data' / 'Document'
-# N = 2265
-
-
-class BaseLoader:
-    def __init__(self, path: Path, file_name: str):
-        self.real_file_path = path / file_name
-        self.start_line = 0
-
-    @abc.abstractmethod
-    def read(self):
-        pass
-
-
-class ListLoader(BaseLoader):
-
-    def read(self):
-        with open(str(self.real_file_path), 'r') as fp:
-            text = fp.read()
-        return text
-
-    def get_list(self):
-        all_list = []
-        for line in self._iter_line_list():
-            list_in_line = line.split()
-            all_list.extend(list_in_line)
-        return all_list
-
-    def set_start_line(self, start_line: int):
-        self.start_line = start_line
-
-    def _iter_line_list(self) -> str:
-        with open(str(self.real_file_path), 'r') as fp:
-            line_list = fp.readlines()
-
-        wanted_list = line_list[self.start_line:]
-        for line in wanted_list:
-            yield line.strip()
-
-
-class TermLoader(BaseLoader):
-
-    def read(self):
-        with open(str(self.real_file_path), 'r') as fp:
-            text = fp.read()
-        return text
-
-    def iter_term(self, skip_last_token: bool = True):
-        for line in self._iter_line_list():
-            term_in_line = line.split()
-            term_in_line = term_in_line[:-1] if skip_last_token else term_in_line
-            for term in term_in_line:
-                yield term
-
-    def set_start_line(self, start_line: int):
-        self.start_line = start_line
-
-    def _iter_line_list(self) -> str:
-        with open(str(self.real_file_path), 'r') as fp:
-            line_list = fp.readlines()
-
-        wanted_list = line_list[self.start_line:]
-        for line in wanted_list:
-            yield line.strip()
 
 
 if __name__ == '__main__':
